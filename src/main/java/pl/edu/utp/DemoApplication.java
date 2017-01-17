edcd65798ae7ef7398a417ef7574759e9a11124c
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2017-01-17 22:49:21 +0100

    Update - nie działa zapisywanie do sesji

--- src/main/java/pl/edu/utp/DemoApplication.java
+++ src/main/java/pl/edu/utp/DemoApplication.java
@@ -2,12 +2,15 @@ package pl.edu.utp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
+@EnableOAuth2Client
+@RestController
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/pl/edu/utp/SecurityConfiguration.java
+++ src/main/java/pl/edu/utp/SecurityConfiguration.java
@@ -1,16 +1,37 @@
 package pl.edu.utp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, proxyTargetClass = true)
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends GlobalMethodSecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,13 +44,74 @@ public class SecurityConfiguration extends GlobalMethodSecurityConfiguration {
         //@formatter:on
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.antMatcher("/**")
+//                .authorizeRequests()
+//                .antMatchers("/", "/login**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf().disable();
+
+
+        http.csrf().disable(); // Use Vaadin's built-in CSRF protection instead
+
+        http
+                .authorizeRequests()
+                .antMatchers("/login/**","/**").anonymous()
+                .antMatchers("/vaadinServlet/UIDL/**", "/**").permitAll()
+                .antMatchers("/vaadinServlet/HEARTBEAT/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);;
+//        http.httpBasic().disable();
+//        http.formLogin().disable();
+        http.exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+
+    }
+
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return authenticationManager();
     }
 
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**", "/VAADIN/**", "/vaadinServlet/**", "HEARTBEAT/**", "/vaadinServlet");
+    }
+
     static {
         // Use a custom SecurityContextHolderStrategy
-        SecurityContextHolder.setStrategyName(VaadinSessionSecurityContextHolderStrategy.class.getName());
+//        SecurityContextHolder.setStrategyName(VaadinSessionSecurityContextHolderStrategy.class.getName());
     }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
+        return facebookFilter;
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook.client")
+    public AuthorizationCodeResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook.resource")
+    public ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(
+            OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
 }
